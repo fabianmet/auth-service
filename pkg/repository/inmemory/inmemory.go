8fd170b93de55d609da76af0eebe678a3ef105a1
fabianmet <f.met@fullstaq.com>
2021-04-05 12:45:46 +0200

    Writing a simple in memory store.

---
+++ pkg/repository/inmemory/inmemory.go
@@ -0,0 +1,83 @@
+//package inmemory contains an inmemory store for our usecase.
+// ONLY TO BE USED IN DEVELOPMENT!!
+package inmemory
+
+import "errors"
+
+//InMemoryClient is an in memory client that stores objects in memory.
+type InMemoryClient struct {
+	contents []inMemoryObject
+}
+
+//inMemoryObject contains keys and byteArrays that we use to store our data.
+type inMemoryObject struct {
+	key       string
+	byteArray []byte
+}
+
+//NewInMemoryClient returns an empty InMemoryClient used for CRUD on keys.
+func NewInMemoryClient() *InMemoryClient {
+	return &InMemoryClient{
+		contents: []inMemoryObject{},
+	}
+}
+
+//Read reads from the inmemory client and returns the found key
+func (i *InMemoryClient) Read(s string) ([]byte, error) {
+	ind, err := i.findInSlice(s)
+	if err != nil {
+		return nil, err
+	}
+	return i.contents[ind].byteArray, nil
+}
+
+//Delete removes from inmemory store. Its not the nicest but hey its only for development cases! DO NOT USE IN PRODUCTION
+func (i *InMemoryClient) Delete(s string) error {
+
+	ind, err := i.findInSlice(s)
+	if err != nil {
+		return err
+	}
+
+	// this deletes an item from a slice, at the index.
+	i.contents[ind] = i.contents[len(i.contents)-1]
+	i.contents[len(i.contents)-1] = inMemoryObject{}
+	i.contents = i.contents[:len(i.contents)-1]
+	return nil
+}
+
+func (i *InMemoryClient) Create(s string, b []byte) error {
+	content := inMemoryObject{
+		key:       s,
+		byteArray: b,
+	}
+	i.contents = append(i.contents, content)
+	return nil
+}
+
+func (i *InMemoryClient) UpdateKey(s string, b []byte) error {
+
+	ind, err := i.findInSlice(s)
+	if err != nil {
+		return err
+	}
+
+	content := inMemoryObject{
+		key:       s,
+		byteArray: b,
+	}
+
+	i.contents[ind] = content
+
+	return nil
+}
+
+//findInSlice will search through the inmemory store for the object and if it is found return the index, OR error out with an index of -1
+func (i *InMemoryClient) findInSlice(s string) (int, error) {
+	for ind, v := range i.contents {
+		if v.key == s {
+			return ind, nil
+		}
+	}
+	return -1, errors.New("repository: key not found")
+}
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,1 +1,18 @@
+//package repository contains all backend repositories for the authentication service.
+// working on redis and an in memory one.
+// this package exposes the ways to crud rsa keys for token generation and validation, and user data.
 package repository
+
+import (
+	"github.com/fabianmet/auth-service/pkg/repository/inmemory"
+)
+
+type Repository interface {
+	Read(string) ([]byte, error)
+	Delete(string) error
+	Create(string, []byte) error
+	UpdateKey(string, []byte) error
+}
+
+// interface checking!
+var _ Repository = (*inmemory.InMemoryClient)(nil)
